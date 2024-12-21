be6f179312e798e3ea7b43bff32666fdea5cb9b6
mohmmadali9091@gmail.com <mohmmadali9091@gmail.com>
2024-12-21 09:36:31 +0500

    Your commit message

--- js/index.js
+++ js/index.js
@@ -19,17 +19,30 @@ async function fetchData() {
           ? productApi[i].description.substring(0, 100) + "..."
           : productApi[i].description;
 
-      productSection.innerHTML += `<div class="card" style="width: 18rem">
-        <img src="${productApi[i].image}" class="card-img-top" alt="..." 
-             style="height: 200px; width: auto; object-fit: contain;" />
-        <div class="card-body">
-          <h5 class="card-title">${truncatedTitle}</h5>
-          <p class="card-text">
-          ${truncatedDescription}
-          </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>`;
+      // productSection.innerHTML += `<div class="card" style="width: 18rem ">
+      //   <img src="${productApi[i].image}" class="card-img-top" alt="..." 
+      //        style="height: 200px; width: auto; object-fit: contain;" />
+      //   <div class="card-body">
+      //     <h5 class="card-title">${truncatedTitle}</h5>
+      //     <p class="card-text">
+      //     ${truncatedDescription}
+      //     </p>
+      //     <a href="#" class="btn btn-primary">Go somewhere</a>
+      //   </div>
+      // </div>`;
+      productSection.innerHTML += `
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="card h-100">
+      <img src="${productApi[i].image}" class="card-img-top" alt="..." 
+           style="height: 200px; width: auto; object-fit: contain;" />
+      <div class="card-body">
+        <h5 class="card-title">${truncatedTitle}</h5>
+        <p class="card-text">${truncatedDescription}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>`;
+
     }
   } catch (error) {
     console.log(error);
@@ -37,3 +50,76 @@ async function fetchData() {
 }
 
 fetchData();
+
+
+// signup js start
+var userName = document.getElementById("username");
+var userEamil = document.getElementById("email");
+var userPass = document.getElementById("password");
+
+
+function signupFunc() {
+    if (userName.value == "" || !userEamil.value || !userPass.value) {
+      alert("Please fill all the fields");
+      return;
+    }
+    let usersArray = localStorage.getItem("users") || [];
+    console.log("wiuout parse", usersArray);
+    if (usersArray.length != 0) {
+      usersArray = JSON.parse(usersArray);
+    }
+    for (var i = 0; i < usersArray.length; i++) {
+      if (userEamil.value == usersArray[i].userEamil) {
+        alert("Email already in use");
+        return;
+      }
+    }
+    const userObj = {
+      userName: userName.value,
+      userEamil: userEamil.value,
+      userPass: userPass.value,
+    };
+    usersArray.push(userObj);
+    localStorage.setItem("users", JSON.stringify(usersArray));
+    userName.value = "";
+    userEamil.value = "";
+    userPass.value = "";
+    alert("Signed up successfully");
+  }
+// signup js end
+
+
+// login js start
+
+
+var loginUsername = document.getElementById("loginUsername");
+var loginPassword = document.getElementById("loginPassword");
+
+
+
+
+
+function loginFunc() {
+    let usersArray = localStorage.getItem("users");
+    if (usersArray == null) {
+      alert("You are not signed up");
+      return;
+    }
+    usersArray = JSON.parse(usersArray);
+    var flag = false;
+    for (var i = 0; i < usersArray.length; i++) {
+      if (
+        loginUsername.value == usersArray[i].userName &&
+        loginPassword.value == usersArray[i].userPass
+      ) {
+        flag = true;
+        alert("Logged in successfully");
+        window.location.replace("/index.html");
+      }
+    }
+    if (!flag) {
+      alert("Please enter correct credentials");
+    }
+  }
+
+// login js end
